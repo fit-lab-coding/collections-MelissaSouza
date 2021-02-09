24ad6032847afa60c42d6b7c59d32bf3874b014e
MelissaSouza <31051552+MelissaSouza@users.noreply.github.com>
2021-02-08 22:14:50 -0300

    Update and rename AttendenceHelper.java to AttendanceHelper.java

--- src/main/java/fit/core/AttendanceHelper.java
+++ src/main/java/fit/core/AttendanceHelper.java
@@ -3,18 +3,18 @@ package fit.core;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AttendenceHelper {
+public class AttendanceHelper {
   public static Map<String, Boolean> random(int students, int percentage){
-    Map<String, Boolean> attendanceList = new HashMap<>(); 
+    Map<String, Boolean> AttendanceList = new HashMap<>(); 
 
   int totalPresence = (int)Math.ceil(students * (percentage/100.00));
 
   for(int i = 0; i < students; i++) {
     boolean present = i < totalPresence;
-    attendanceList.put("" + i, present);
+    AttendanceList.put("" + i, present);
   }
 
-    return attendanceList;
+    return AttendanceList;
 
   }
 }
