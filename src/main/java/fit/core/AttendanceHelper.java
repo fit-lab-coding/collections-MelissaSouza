614d10f68703a6041f12e4f92a59f81b1801b9a3
MelissaSouza <31051552+MelissaSouza@users.noreply.github.com>
2021-02-09 00:21:27 -0300

    Attendance helper correção

--- src/main/java/fit/core/AttendanceHelper.java
+++ src/main/java/fit/core/AttendanceHelper.java
@@ -4,17 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AttendanceHelper {
-  public static Map<String, Boolean> random(int students, int percentage){
-    Map<String, Boolean> AttendanceList = new HashMap<>(); 
+    public static Map<String, Boolean> random2(int amount, int percentage) {
+        int result = (amount * percentage) / 100;
 
-  int totalPresence = (int)Math.ceil(students * (percentage/100.00));
+        Map<String, Boolean> listUser = new HashMap<>();
 
-  for(int i = 0; i < students; i++) {
-    boolean present = i < totalPresence;
-    AttendanceList.put("" + i, present);
-  }
+        for (int i = 0; i < result; i++) {
+            listUser.put("user" + i, true);
+        }
 
-    return AttendanceList;
+        for (int i = 0; i < amount - result; i++) {
+            listUser.put("userTotal" + i, false);
+        }
 
-  }
+
+        return listUser;
+    }
+
+    public static Map<String, Boolean> random(int students, int presentPercentile) {
+        Map<String, Boolean> attendanceList = new HashMap<>();
+
+        int totalPresence = (int)Math.ceil(students * (presentPercentile / 100.00));
+
+        for (int i = 0; i < totalPresence; i++) {
+            boolean present = i < totalPresence;
+            attendanceList.put("" + i, present);
+        }
+
+        return attendanceList;
+    }
 }
